01776d7f7cb511245966814fb4e5f49563b6bbc2
Sarah Waldie <sarahwaldie@Sarahs-MacBook-Air.local>
2018-05-03 10:34:19 -0400

    added semantic element main and all placeholder image srcsets

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,7 +14,47 @@ function navShrink() {
             $(".sticky-nav").removeClass("shrink");
         }
     });
+};
+
+
+//Smooth scroll instead of jumping to anchor links.
+
+function smoothScroll() {
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 54
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        }
+                    });
+                }
+            }
+        });
 }
 
-window.onload = navShrink;
-
+window.addEventListener("load", navShrink);
+window.addEventListener("load", smoothScroll);
